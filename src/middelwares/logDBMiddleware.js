02d0383313a1fc6cf04b37a7ecb18af47c3cf67c
frangomar <frangomar8@gmail.com>
2021-11-18 15:10:02 -0300

    instale brcrypt que no me figuraba y borre los middleware de log y los txt

--- src/middelwares/logDBMiddleware.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-const logDBMiddleware = (req, res, next) => {
-    fs.appendFileSync('logDB.txt', 'Se creo un registro al ingreso en la pagina ' + req.url);
-
-    next();
-};
-module.exports = logDBMiddleware;
--- src/middelwares/logMiddlewares.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-const logMiddleware = (req, res, next) => {
-    fs.appendFileSync('log.txt', 'Se ingreso en la pagina ' + req.url);
-
-    next();
-};
-module.exports = logMiddleware;
