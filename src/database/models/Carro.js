6119e4dd29a47ae0c51ae2766e9a0f30ac5361fa
frangomar <frangomar8@gmail.com>
2021-11-24 15:16:22 -0300

    se agrego modelo carrito de compras y borro linea demas en create de frutosSecos

---
+++ src/database/models/Carro.js
@@ -0,0 +1,46 @@
+
+module.exports = (sequelize, DataTypes) => {
+    let alias = 'Carros';
+    let cols = {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        product: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+               
+    };
+    let config = {
+        tableName: 'carts',
+        timestamps: false
+    }
+
+    const Carro = sequelize.define(alias, cols, config);
+
+    Carro.associate = function(models) {
+        Carro.belongsTo(models.Usuarios, {
+            as: 'usuarios',
+            foreignKey: 'user_id'
+        })
+        Carro.belongsToMany(models.Productos, {
+            as: 'productos',
+            through: 'cart_product',
+            foreignKey:'cart_id',
+            otherKey: 'product_id',
+            timestamp: false
+        })
+    }
+    return Usuario;
+};
