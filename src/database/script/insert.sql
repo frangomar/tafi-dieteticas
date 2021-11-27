64e558402b523cdf71cf36fc97981c456ee418a3
frangomar <frangomar8@gmail.com>
2021-11-27 18:13:43 -0300

    cargue mas productos en el script de carga

--- src/database/script/insert.sql
+++ src/database/script/insert.sql
@@ -70,3 +70,23 @@ where id = 11;
 update products
 set image = 'mix-orann.png'
 where id = 12;
+insert into products (title, description, price, image, category_id)
+values ('Le-fit','Mantequilla de mani',130,'mantequilla-mani.png', 1);
+insert into products (title, description, price, image, category_id)
+values ('Bebida de Soja','Leche de Soja',130,'leche-soja.png', 1);
+insert into products (title, description, price, image, category_id)
+values ('Mayonesa Mi Soja','Mayonesa de soja',130,'mayonesa-soja.png', 1);
+insert into products (title, description, price, image, category_id)
+values ('Chiapaz','galletas de chia',130,'galletas-chiapaz.png', 2);
+insert into products (title, description, price, image, category_id)
+values ('Tanlatam','galletas de cacao, mani y cafe',130,'galletas-tanlatam.png', 2);
+insert into products (title, description, price, image, category_id)
+values ('Linocco','galletas de lino',130,'galletas-linocco.png', 2);
+insert into products (title, description, price, image, category_id)
+values ('Medallon lentejas','medallones de lenteja',130,'medallon-lenteja.png', 2);
+insert into products (title, description, price, image, category_id)
+values ('Medallon menta','medallones de menta',130,'medallon-menta.png', 2);
+insert into products (title, description, price, image, category_id)
+values ('Medallon quinoa','medallones de quinoa',130,'medallon-quinoa.png', 2);
+insert into products (title, description, price, image, category_id)
+values ('Mix Energetico','medallones oran',130,'medallon-lenteja.png', 2);
